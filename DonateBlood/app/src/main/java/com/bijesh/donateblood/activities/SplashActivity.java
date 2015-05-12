d5930b96637a796f562c79b967486b9ecff0374a
unknown <bijesh.jayaprakash@gmail.com>
2015-05-12 21:04:32 +0530

    images and BaseActivity

---
+++ DonateBlood/app/src/main/java/com/bijesh/donateblood/activities/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.bijesh.donateblood.activities;
+
+import android.os.Bundle;
+
+import com.bijesh.donateblood.base.ui.BaseActivity;
+
+/**
+ * Created by Bijesh on 12-05-2015.
+ */
+public class SplashActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
---
+++ DonateBlood/app/src/main/java/com/bijesh/donateblood/base/ui/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.bijesh.donateblood.base.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by Bijesh on 12-05-2015.
+ */
+public class BaseActivity extends Activity {
+}
