613db1c5fbaf196cb8976405b8e40d407f119fa7
unknown <bijesh.jayaprakash@gmail.com>
2015-05-24 12:38:19 +0530

    init admob to app

--- DonateBlood/app/src/main/java/com/bijesh/donateblood/activities/SponserActivity.java
+++ DonateBlood/app/src/main/java/com/bijesh/donateblood/activities/SponserActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 
 import com.bijesh.donateblood.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 /**
  * Created by Bijesh on 24-05-2015.
@@ -15,14 +17,30 @@ public class SponserActivity extends ActionBarActivity {
 
     private static final String TAG = SponserActivity.class.getCanonicalName();
     private Toolbar mToolBar;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sponser);
 
-        initToolBar();
+        mAdView = (AdView) findViewById(R.id.ad_view);
 
+        initToolBar();
+        initAd();
+
+    }
+
+    private void initAd(){
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("CC4ABA2CBA9C4B77B9C08ADA12E4817E")
+                .build();
+
+        // Start loading the ad in the background.
+        mAdView.loadAd(adRequest);
     }
 
     private void initToolBar(){
@@ -39,4 +57,35 @@ public class SponserActivity extends ActionBarActivity {
             }
         });
     }
+
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+
+
+
+
 }
